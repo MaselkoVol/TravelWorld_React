f518f2ecb97e3ea524d7555f3368d24a4f701253
Володимир Маселко <kartoshka8800@gmail.com>
2024-07-12 15:13:40 +0300

    working slider without buttons or navigation, no clusters

--- src/componenets/UI/slider/Slider.tsx
+++ src/componenets/UI/slider/Slider.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from 'react'
+import React, { useRef, useEffect, useState, useReducer } from 'react'
 import "./Slider.scss";
 
 type BaseProps = {
@@ -15,72 +15,204 @@ type Props =
 	| BaseProps & { amount?: number; elemMinWidth: number };
 
 function Slider({ elements, className, gap = 0, elemMinWidth, amount }: Props) {
+	const [isComponentReady, setIsComponentReady] = useState(false);
 
-	const [elementsWidth, setElementsWidth] = useState("0px");
-	const [imagesAmountOnPage, setImagesAmountOnPage] = useState(0);
+	const [elementsWidth, setElementsWidth] = useState(0);
+	const elementsWidthRef = useRef(elementsWidth);
+	const setElementsWidthRef = (value: number) => {
+		setElementsWidth(value);
+		elementsWidthRef.current = value;
+	}
+
+	function getElemStyles(idx: number) {
+		const basicElemStyles = {
+			minWidth: elementsWidth + "px",
+		}
+		let fullElemStyles = null;
+		if (gap) {
+			fullElemStyles = {
+				...basicElemStyles,
+				borderLeft: `${(idx == 0) ? 0 : gap}px solid transparent`,
+			}
+		} else if (idx == 0) {
+			fullElemStyles = {
+				...basicElemStyles,
+				borderLeft: "none",
+			}
+		} else {
+			fullElemStyles = {
+				...basicElemStyles,
+			}
+		}
+		return fullElemStyles;
+	}
+
+	const elementsAmountOnPage = useRef(0);
+	const currentElementOnScreen = useRef(0);
+	const isDragStart = useRef(false);
+	const isDraggable = useRef(false);
+	const prevPageX = useRef(0);
+	const prevScrollLeft = useRef(0);
+	const positionDiff = useRef(0);
 
 	const sliderRef = useRef<HTMLDivElement>(null);
 	const containerRef = useRef<HTMLDivElement>(null);
 
-	useEffect(() => {
-		const container = containerRef.current;
+	function scrollImmediatelyToCurrentPage(container: HTMLElement) {
+		container.classList.add("dragging");
+		setTimeout(() => {
+			const currentElement = container.children[currentElementOnScreen.current] as HTMLElement;
+			container.scrollLeft = currentElement.offsetLeft + currentElement.clientLeft;
+			setTimeout(() => { container.classList.remove("dragging") }, 0);
+		}, 0);
+	}
 
-		if (!container) return;
+	function resizeElementsWith(container: HTMLElement) {
+		const lastChild = container.lastElementChild as HTMLElement;
+		const myGap = lastChild.clientLeft;
 
-		// if amount is passed, 
-		if (amount) {
-			setElementsWidth((container.offsetWidth - gap * (amount - 1)) / amount + "px");
-			setImagesAmountOnPage(amount)
-		}
-
-		// if minWidth is passed
-		let resizeObserver: (ResizeObserver | null) = null;
 		if (elemMinWidth) {
-			resizeObserver = new ResizeObserver((entries) => {
-				resizeElementsWithMinWidth(container);
-			});
-
-			resizeObserver.observe(container);
-		}
-
-		return () => {
-			// if minWidth is passed
-			if (resizeObserver) {
-				resizeObserver.disconnect();
-			}
-		};
-	}, [])
-
-	// if min width is spesified, this function checks how many images fits on the screen and resizes
-	function resizeElementsWithMinWidth(container: HTMLElement) {
-		if (elemMinWidth) {
-			let amountWhichFit = Math.floor((container.offsetWidth + gap) / (elemMinWidth + gap));
+			let amountWhichFit = Math.floor((container.offsetWidth + myGap) / (elemMinWidth + myGap));
 			// it means only one image can be on page
 			if (container.offsetWidth <= elemMinWidth) {
 				amountWhichFit = 1;
 			}
-			let newWidth = (container.offsetWidth - gap * (amountWhichFit - 1)) / amountWhichFit;
+			let newWidth = (container.offsetWidth - myGap * (amountWhichFit - 1)) / amountWhichFit;
 
-			setImagesAmountOnPage(amountWhichFit);
+			elementsAmountOnPage.current = amountWhichFit;
 			if (window.innerWidth < newWidth) {
-				setElementsWidth("100%");
-				return;
+				setElementsWidthRef(container.clientWidth);
+			} else {
+				setElementsWidthRef(newWidth);
 			}
-			setElementsWidth(newWidth + "px");
 		}
+
+		if (amount) {
+			setElementsWidthRef((container.offsetWidth - myGap * (amount - 1)) / amount);
+			elementsAmountOnPage.current = amount;
+		}
+
+		scrollImmediatelyToCurrentPage(container);
 	}
 
+	function defineElementsWidth(container: HTMLElement) {
+		let resizeObserver = new ResizeObserver(() => {
+			resizeElementsWith(container);
+		});;
+		resizeObserver.observe(container);
+		// if amount is passed, 
+		return resizeObserver;
+	};
+
+
+	function dragStart(e: MouseEvent | TouchEvent, container: HTMLElement) {
+		// updatating global variables value on mouse down event
+		isDragStart.current = true;
+		if (e instanceof MouseEvent) {
+			prevPageX.current = e.pageX;
+		} else {
+			prevPageX.current = e.touches[0].pageX;
+		}
+		prevScrollLeft.current = container.scrollLeft;
+	};
+
+	function dragging(e: MouseEvent | TouchEvent, container: HTMLElement) {
+		// scrolling images/carousel to left according to mouse pointer
+		if (!isDragStart.current) return;
+		e.preventDefault();
+		isDraggable.current = true;
+		container.classList.add("dragging");
+		positionDiff.current = 0;
+		if (e instanceof MouseEvent) {
+			positionDiff.current = e.pageX - prevPageX.current;
+		} else {
+			positionDiff.current = e.touches[0].pageX - prevPageX.current;
+		}
+		container.scrollLeft = prevScrollLeft.current - positionDiff.current;
+	};
+
+	function findClosestElement(container: HTMLElement): number {
+		const elementsHTML = Array.from(container.children) as HTMLElement[];
+		let closestElementPos = 0;
+		for (let i = 0; i < elementsHTML.length; i++) {
+			const elemDistanceToStart = elementsHTML[i].offsetLeft + elementsHTML[i].clientLeft;
+			let elemPervDistanceToStart = 0;
+			if (i !== 0) {
+				elemPervDistanceToStart = elementsHTML[i - 1].offsetLeft + elementsHTML[i - 1].clientLeft;
+			}
+			if (elemDistanceToStart < container.scrollLeft && i !== elementsHTML.length - 1) {
+				continue;
+			}
+			if (i !== 0 && container.scrollLeft - elemPervDistanceToStart < elemDistanceToStart - container.scrollLeft) {
+				closestElementPos = elemPervDistanceToStart;
+				currentElementOnScreen.current = i - 1;
+			} else {
+				closestElementPos = elemDistanceToStart;
+				currentElementOnScreen.current = i;
+			}
+			break;
+		}
+		return closestElementPos;
+	}
+
+	function autoSlide(container: HTMLElement) {
+		let closestElementPos = findClosestElement(container);
+		container.scrollLeft = closestElementPos;
+	};
+
+	const dragStop = (container: HTMLElement) => {
+		isDragStart.current = false;
+		container.classList.remove("dragging");
+		if (!isDraggable.current) return;
+		isDraggable.current = false;
+		autoSlide(container);
+	};
+
+
+	useEffect(() => {
+		const container = containerRef.current;
+		if (!container) return;
+
+		const resizeObserver = defineElementsWidth(container);
+
+		const dragStartArgs = (e: MouseEvent | TouchEvent) => (dragStart(e, container));
+		const draggingArgs = (e: MouseEvent | TouchEvent) => (dragging(e, container));
+		const dragStopArgs = () => (dragStop(container));
+
+		container.addEventListener("mousedown", dragStartArgs);
+		container.addEventListener("touchstart", dragStartArgs);
+		document.addEventListener("mousemove", draggingArgs);
+		container.addEventListener("touchmove", draggingArgs);
+		document.addEventListener("mouseup", dragStopArgs);
+		container.addEventListener("touchend", dragStopArgs);
+
+		setTimeout(() => setIsComponentReady(true), 5);
+
+		return () => {
+			if (resizeObserver) {
+				resizeObserver.disconnect();
+			}
+
+			container.removeEventListener("mousedown", dragStartArgs);
+			container.removeEventListener("touchstart", dragStartArgs);
+			document.removeEventListener("mousemove", draggingArgs);
+			container.removeEventListener("touchmove", draggingArgs);
+			document.removeEventListener("mouseup", dragStopArgs);
+			container.removeEventListener("touchend", dragStopArgs);
+		};
+	}, []);
 
 	return (
 		<div ref={sliderRef} className={`super-duper-slider ${className ? className : ""}`}>
-			<div style={{ gap: gap + "px" }} ref={containerRef} className='super-duper-slider__container'>
+			<div ref={containerRef} className='super-duper-slider__container'>
 				{elements.map((elem, idx) => (
-					<div key={idx} style={{ minWidth: elementsWidth }} className='super-duper-slider__element-container'>
-						{elem}
+					<div key={idx} className='super-duper-slider__element-container'
+						style={getElemStyles(idx)} >
+						{isComponentReady ? elem : <div></div>}
 					</div>
 				))}
 			</div>
-		</div>
+		</div >
 	)
 }
 
--- src/pages/aboutPage/AboutPage.tsx
+++ src/pages/aboutPage/AboutPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./AboutPage.scss";
 import { useHeaderType } from '../../App';
 import Slider from '../../componenets/UI/slider/Slider';
@@ -8,19 +8,19 @@ type Props = {}
 function AboutPage({ }: Props) {
 
 	let images = [
-		"https://images.pexels.com/photos/3680219/pexels-photo-3680219.jpeg?cs=srgb&dl=pexels-lukas-rodriguez-1845331-3680219.jpg&fm=jpg",
-		"https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w1200/2023/10/free-images.jpg",
 		"https://cdn.pixabay.com/photo/2023/12/06/21/07/photo-8434386_1280.jpg",
-		"https://wallpapers.com/images/featured/hd-a5u9zq0a0ymy2dug.jpg",
+		"https://cdn.pixabay.com/photo/2023/12/06/21/07/photo-8434386_1280.jpg",
+		"https://cdn.pixabay.com/photo/2023/12/06/21/07/photo-8434386_1280.jpg",
+		"https://cdn.pixabay.com/photo/2023/12/06/21/07/photo-8434386_1280.jpg",
 
-		"https://static.vecteezy.com/system/resources/thumbnails/036/135/743/small_2x/ai-generated-colored-water-drops-on-abstract-background-water-drops-on-colorful-background-colored-wallpaper-ultra-hd-colorful-wallpaper-background-with-colored-bubbles-photo.jpg",
-		"https://wallpapers.com/images/hd/best-hd-autumn-leaves-57tbt9tq3xf3vdm2.jpg",
-		"https://images.pexels.com/photos/1563355/pexels-photo-1563355.jpeg?cs=srgb&dl=pexels-thatguycraig000-1563355.jpg&fm=jpg",
-		"https://static.vecteezy.com/system/resources/previews/030/355/618/non_2x/flowers-in-the-field-mountains-flowers-nature-nature-hd-wallpaper-ai-generated-free-photo.jpg",
+		"https://cdn.pixabay.com/photo/2023/12/06/21/07/photo-8434386_1280.jpg",
+		"https://cdn.pixabay.com/photo/2023/12/06/21/07/photo-8434386_1280.jpg",
+		"https://cdn.pixabay.com/photo/2023/12/06/21/07/photo-8434386_1280.jpg",
+		"https://cdn.pixabay.com/photo/2023/12/06/21/07/photo-8434386_1280.jpg",
 
 	];
 	const imageElements = images.map((image, idx) => (
-		<Image key={idx} src={image} className='about-page__image' />
+		<Image draggable={false} key={idx} src={image} className='about-page__image' />
 	));
 
 	const setHeaderType = useHeaderType();
@@ -29,8 +29,7 @@ function AboutPage({ }: Props) {
 	}, [])
 	return (
 		<div className='container'>
-
-			<Slider gap={45} elements={imageElements} elemMinWidth={300} />
+			<Slider gap={10} elements={imageElements} amount={3} />
 		</div>
 	)
 }
